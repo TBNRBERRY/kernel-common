5ced901fc60d4286eff2e63fcadd5d5769f51d5d
TBNRBERRY <100874438+TBNRBERRY@users.noreply.github.com>
2025-05-12 10:56:46 -0500

    Updated exec.c
    Changed line 1235
    From: 
    strncpy(buf, tsk->comm, sizeof(buf));
    
    To: 
    strncpy(buf, tsk->comm, TASK_COMM_LEN);
    buf[TASK_COMM_LEN - 1] = '\0'; // Ensure null-termination

--- fs/exec.c
+++ fs/exec.c
@@ -1232,7 +1232,8 @@ char *get_task_comm(char *buf, struct task_struct *tsk)
 {
 	/* buf must be at least sizeof(tsk->comm) in size */
 	task_lock(tsk);
-	strncpy(buf, tsk->comm, sizeof(buf));
+	strncpy(buf, tsk->comm, TASK_COMM_LEN);
+	buf[TASK_COMM_LEN - 1] = '\0'; // Ensure null-termination
 	task_unlock(tsk);
 	return buf;
 }
